732fbd47e370423892e84fd5fbacf31fda224ba9
atoroto <9368370+atoroto@users.noreply.github.com>
2026-06-15 01:26:39 +0300

    move around

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,14 +13,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-    screen.fill("black")
-    pygame.display.flip()
+        dt = clock.tick(60) / 1_000
+        # print(dt)
+
+        screen.fill("black")
+
+        player.update(dt)
+        player.draw(screen)
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
